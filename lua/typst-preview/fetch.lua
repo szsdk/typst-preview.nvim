985a6f8917887f12af34e1e4b3d9407a8baffb00
szsdk <shenz34206@hotmail.com>
2024-12-25 17:17:54 +0100

    update tinymist to 0.12.14

--- lua/typst-preview/fetch.lua
+++ lua/typst-preview/fetch.lua
@@ -180,7 +180,7 @@ end
 function M.bins_to_fetch()
   return {
     {
-      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.4/'
+      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.14/'
         .. M.get_tinymist_bin_name(),
       bin_name = M.get_tinymist_bin_name(),
       name = 'tinymist',
